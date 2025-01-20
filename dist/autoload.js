0fbd673ca868caf48cb22ab992a9e2f5dedea2e1
待抉 <1253876012@qq.com>
2025-01-20 11:12:49 +0800

    1.0.5

--- dist/autoload.js
+++ dist/autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/ffy6511/live2d-widget@v1.0.2/dist/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/ffy6511/live2d-widget@v1.0.5/dist/";
 //const live2d_path = "http://127.0.0.1:8080/dist/";
 
 // 封装异步加载资源的方法
@@ -26,25 +26,19 @@ function loadExternalResource(url, type) {
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
-    Promise.all([
-        loadExternalResource(live2d_path + "waifu.css", "css"),
-        loadExternalResource(live2d_path + "live2d.min.js", "js"),
-        loadExternalResource(live2d_path + "waifu-tips.js", "js")
-    ]).then(() => {
-        // 等待一小段时间确保 waifu-tips.js 完全执行
-        setTimeout(() => {
-            // 确保 initWidget 函数存在
-            if (typeof initWidget === "function") {
-                initWidget({
-                    waifuPath: live2d_path + "waifu-tips.json",
-                    cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
-                    tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
-                });
-            } else {
-                console.error("initWidget is not defined, waifu-tips.js might not be loaded correctly");
-            }
-        }, 1000);
-    });
+	Promise.all([
+		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+	]).then(() => {
+		// 配置选项的具体用法见 README.md
+		initWidget({
+			waifuPath: live2d_path + "waifu-tips.json",
+			//apiPath: "https://live2d.fghrsh.net/api/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
+		});
+	});
 }
 
 console.log(`
